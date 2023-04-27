08e2b10fb343603d42c006c1c098c82757af8d0b
JisooPyo <audwl326@gmail.com>
2023-04-27 23:29:09 +0900

    test230427

---
+++ 2304/test230427/Basic134.java
@@ -0,0 +1,23 @@
+package test230427;
+
+import java.util.Arrays;
+
+public class Basic134 {
+
+	public static void main(String[] args) {
+		Basic134 b = new Basic134();
+		
+		System.out.println(Arrays.deepToString(b.solution(4)));
+		System.out.println(Arrays.deepToString(b.solution(5)));
+
+	}
+	
+    public int[][] solution(int n) {
+        int[][] answer = {};
+        
+        return answer;
+    }
+	
+	
+
+}
