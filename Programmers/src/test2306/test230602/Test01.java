5feaf3d807ae4cdfbc9936c72b33cb8da4017c21
JisooPyo <audwl326@gmail.com>
2023-06-03 17:21:21 +0900

    test230603

--- Programmers/src/test2306/test230602/Test01.java
+++ Programmers/src/test2306/test230602/Test01.java
@@ -5,13 +5,6 @@ import java.util.ArrayList;
 public class Test01 {
     public static void main( String[] args ) {
         String[] a = new String[] { "a", "b", "c", "d" };
-        String[] b = new String[] { "a", "b", "c", "d" };
-        String c = "a";
-        String d = "a";
-        System.out.println( a.equals( b ) );
-        System.out.println( c.equals( d ) );
-
-
         ArrayList< String[] > list = new ArrayList<>();
         list.add( new String[] { "a", "b", "c", "d" } );
         System.out.println( list.contains( a ) );   // false
